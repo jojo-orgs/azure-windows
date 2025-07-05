8dde4bf7bd75e90e4b22cb79e475f7a950e9b0e4
jojo-engineer <mcukoha@gmail.com>
2025-07-05 23:56:42 +0100

    modules-introduced

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "vm_name" {
-  value       = [for vm in azurerm_windows_virtual_machine.main : vm.name]
+  value       = azurerm_windows_virtual_machine.main.name
   description = "The name of the Windows virtual machine."
 }
 
 output "vm_id" {
-  value       = [for vm in azurerm_windows_virtual_machine.main : vm.id]
+  value       = azurerm_windows_virtual_machine.main.id
   description = "The ID of the Windows virtual machine."
 }
 
 output "vm_public_ip" {
-  value       = [for vm in azurerm_windows_virtual_machine.main : vm.public_ip_address]
+  value       = azurerm_windows_virtual_machine.main.public_ip_address
   description = "The public IP address of the Windows virtual machine."
 }
 
 output "admin_username" {
-  value       = [for vm in azurerm_windows_virtual_machine.main : vm.admin_username]
+  value       = azurerm_windows_virtual_machine.main.admin_username
   description = "The administrator username for the Windows virtual machine."
 }
 
 output "admin_password" {
-  value       = [for vm in azurerm_windows_virtual_machine.main : vm.admin_password]
+  value       = azurerm_windows_virtual_machine.main.admin_password
   description = "The administrator password for the Windows virtual machine."
   sensitive   = true
 }
